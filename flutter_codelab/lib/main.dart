d178b8ce8a2ac70412bcfaba22d6f3a3bf1b6b96
anilatici <anillatici@gmail.com>
2024-08-21 23:58:15 +0100

    Update the card

--- flutter_codelab/lib/main.dart
+++ flutter_codelab/lib/main.dart
@@ -43,20 +43,21 @@ class MyHomePage extends StatelessWidget {
     // ...
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random AWESOME idea:'),
-          BigCard(pair: pair),
-
-          // ↓ Add this.
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          ),
-
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
+            ),
+        
+          ],
+        ),
       ),
     );
 
@@ -84,7 +85,10 @@ class BigCard extends StatelessWidget {
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asLowerCase, style: style),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}"),
       ),
     );
   }
